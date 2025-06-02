cbfb14a09c2dc2f13e0ab98c03fff27bf45cc04c
wheon06 <up2kid@gmail.com>
2025-06-03 08:40:39 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/wheon/settinglogapi/SettingLogApiApplication.java
@@ -0,0 +1,13 @@
+package com.wheon.settinglogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettingLogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettingLogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wheon/settinglogapi/SettingLogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheon.settinglogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingLogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
