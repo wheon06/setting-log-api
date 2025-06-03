d9005e99336fb41683c4f3d4e66452d61c3b4e63
wheon06 <up2kid@gmail.com>
2025-06-03 15:51:16 +0900

    feat: 성공 시 ID를 포함하는 DefaultIdResult 추가

---
+++ src/main/java/com/wheon/settinglogapi/api/support/DefaultIdResult.java
@@ -0,0 +1,13 @@
+package com.wheon.settinglogapi.api.support;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DefaultIdResult {
+
+    private Long id;
+
+}
