a32557cb6bb95c842381e8a10d92c2aa3f99ec25
wheon06 <up2kid@gmail.com>
2025-06-03 19:33:16 +0900

    feat: CenterRequest Class에서 Record로 변경

--- src/main/java/com/wheon/settinglogapi/api/controller/request/CenterRequest.java
+++ src/main/java/com/wheon/settinglogapi/api/controller/request/CenterRequest.java
@@ -1,19 +1,13 @@
 package com.wheon.settinglogapi.api.controller.request;
 
-import lombok.Getter;
 import com.wheon.settinglogapi.domain.center.Center;
 import jakarta.validation.constraints.NotEmpty;
 
-@Getter
-public class CenterRequest {
-
+public record CenterRequest(
     @NotEmpty(message = "이름을 입력해주세요.")
-    private String name;
-
+    String name
+) {
     public Center toDomain() {
-        return new Center(
-                this.name
-        );
+        return new Center(this.name);
     }
-
 }
