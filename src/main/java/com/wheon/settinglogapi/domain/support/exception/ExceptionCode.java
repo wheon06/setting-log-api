0a0b9d878f49881ddd105dc46b9658f2e354578c
wheon06 <up2kid@gmail.com>
2025-06-03 17:17:39 +0900

    feat: DUPLICATED_CENTER_NAME 추가

--- src/main/java/com/wheon/settinglogapi/domain/support/exception/ExceptionCode.java
+++ src/main/java/com/wheon/settinglogapi/domain/support/exception/ExceptionCode.java
@@ -1,4 +1,4 @@
-package com.wheon.settinglogapi.api.exception;
+package com.wheon.settinglogapi.domain.support.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +9,8 @@ public enum ExceptionCode {
 
     INVALID_REQUEST("E1001", "요청이 잘못되었습니다."),
     INVALID_HTTP_METHOD("E1002", "HTTP Method가 잘못되었습니다."),
+
+    DUPLICATED_CENTER_NAME("E2001", "중복된 센터 이름입니다."),
 
     INTERNAL_SERVER_ERROR("E9999", "서버 오류입니다.");
 
