c771aa75093c8cb126c0a2514d67f4dd2b4d9feb
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-06-04 10:12:04 +0900

    feat: Wall 도메인 생성자 추가

--- src/main/java/com/wheon/settinglogapi/domain/wall/Wall.java
+++ src/main/java/com/wheon/settinglogapi/domain/wall/Wall.java
@@ -1,15 +1,17 @@
 package com.wheon.settinglogapi.domain.wall;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import com.wheon.settinglogapi.domain.center.Center;
 
 @Getter
-@RequiredArgsConstructor
 public class Wall {
 
     private Long id;
     private Center center;
     private String name;
 
+    public Wall(Center center, String name) {
+        this.center = center;
+        this.name = name;
+    }
 }
