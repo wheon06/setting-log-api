b3c180ea1f20368f640062ca5a37c9d7fbd944e6
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-06-04 10:10:42 +0900

    feat: 센터 도메인에 섹터 자식들 추가

--- src/main/java/com/wheon/settinglogapi/domain/center/Center.java
+++ src/main/java/com/wheon/settinglogapi/domain/center/Center.java
@@ -1,12 +1,19 @@
 package com.wheon.settinglogapi.domain.center;
 
 import lombok.Getter;
+import com.wheon.settinglogapi.domain.wall.Wall;
+import java.util.Set;
 
 @Getter
 public class Center {
 
     private Long id;
     private String name;
+    private Set<Wall> walls;
+
+    public void addWall(Wall wall) {
+        this.walls.add(wall);
+    }
 
     public Center(Long id, String name) {
         this.id = id;
